6846b48d42f0065564f5972d9e3f059fe388d101
Nikolay Gorokhov <nikolay.gorokhov@ya.ru>
2024-07-15 12:26:54 +0300

    init

--- app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const totalPrice = ({ price, discount, isInstallment, months }) => {
-    return isInstallment ? (price - (price * discount / 100)) / months : price;
-};
-const price = totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 });
-console.log(price); // 6250
